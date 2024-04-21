3d0a57c1e9e90306ea3b20d577ae978cb160ab65
Mathula <mathula2504@gmail.com>
2024-04-21 10:23:44 +0100

    media message added

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -10,6 +10,8 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:messaging_app/services/auth_service.dart';
 import 'package:messaging_app/services/database_service.dart';
 import 'package:messaging_app/services/media_service.dart';
+import 'package:messaging_app/services/storage_service.dart';
+import 'package:messaging_app/utils.dart';
 
 class ChatPage extends StatefulWidget {
   final UserProfile chatUser;
@@ -25,6 +27,7 @@ class _ChatPageState extends State<ChatPage> {
   late AuthService _authService;
   late DatabaseService _databaseService;
   late MediaService _mediaService;
+  late StorageService _storageService;
   ChatUser? currentUser, otherUser; 
 
   @override
@@ -33,6 +36,8 @@ class _ChatPageState extends State<ChatPage> {
     _authService = _getIt.get<AuthService>();
     _mediaService = _getIt.get<MediaService>();
     _databaseService = _getIt.get<DatabaseService>();
+    _storageService = _getIt.get<StorageService>();
+
     currentUser = ChatUser(
       id: _authService.user!.uid, 
       firstName: _authService.user!.displayName
@@ -84,23 +89,53 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Future<void> _sendMessage(ChatMessage chatMessage)async{
-    Message message = Message(
-      senderID:currentUser!.id, 
-      content: chatMessage.text, 
-      messageType: MessageType.Text, 
-      sentAt: Timestamp.fromDate(chatMessage.createdAt)
-    );
+    if(chatMessage.medias?.isNotEmpty??false){
+      if(chatMessage.medias!.first.type == MediaType.image){
+        Message message = Message(
+          senderID: chatMessage.user.id, 
+          content: chatMessage.medias!.first.url, 
+          messageType: MessageType.Image, 
+          sentAt: Timestamp.fromDate(chatMessage.createdAt),
+        );
+        await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+      }
+    }
+    else{
+      Message message = Message(
+        senderID:currentUser!.id, 
+        content: chatMessage.text, 
+        messageType: MessageType.Text, 
+        sentAt: Timestamp.fromDate(chatMessage.createdAt)
+      );
 
-    await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+      await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+    }
+   
   }
 
   List<ChatMessage> _generateChatMessagesList(List<Message> messages){
     List<ChatMessage> chatMessage = messages.map((m){
-      return ChatMessage(
-        text: m.content!,
-        user: m.senderID==currentUser!.id?currentUser!:otherUser!, 
-        createdAt: m.sentAt!.toDate()
-      );
+      if(m.messageType == MessageType.Image){
+        return ChatMessage(
+          user: m.senderID==currentUser!.id?currentUser!:otherUser!, 
+          createdAt: m.sentAt!.toDate(),
+          medias: [
+            ChatMedia(
+              url: m.content!, 
+              fileName: "", 
+              type: MediaType.image
+            ),
+          ]
+        );
+      }
+      else{
+        return ChatMessage(
+          text: m.content!,
+          user: m.senderID==currentUser!.id?currentUser!:otherUser!, 
+          createdAt: m.sentAt!.toDate()
+        );
+      }
+    
     }).toList();
     chatMessage.sort((a,b){
       return b.createdAt.compareTo(a.createdAt);
@@ -112,6 +147,25 @@ class _ChatPageState extends State<ChatPage> {
     return IconButton(
       onPressed: ()async {
         File? file = await _mediaService.getImageFromGallery();
+        if(file != null){
+          String chatID = generateChatID(
+            uid1: currentUser!.id, 
+            uid2: otherUser!.id,
+          );
+          String? downloadURL = await _storageService.uploadChatImage(
+            file: file, 
+            chatID: chatID
+          );
+          if(downloadURL!=null){
+            ChatMessage chatMessage = ChatMessage(
+              user: currentUser!,
+              createdAt: DateTime.now(),
+              medias: [ChatMedia(url: downloadURL, fileName: "", type: MediaType.image)]
+            );
+
+            _sendMessage(chatMessage);
+          }
+        }
       }, 
       icon: Icon(
         Icons.image,
